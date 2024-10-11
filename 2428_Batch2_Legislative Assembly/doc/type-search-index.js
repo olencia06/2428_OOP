21424c7f75a6deae6fde9c55fa8734141b10d5c7
olencia06 <123053034+olencia06@users.noreply.github.com>
2024-10-11 20:13:23 +0530

    Delete 2428_Batch2_Legislative Assembly/doc directory

--- 2428_Batch2_Legislative Assembly/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"AssemblyManager"},{"p":"<Unnamed>","l":"Bill"},{"p":"<Unnamed>","l":"ChiefMinister"},{"p":"<Unnamed>","l":"GoaLegislativeAssembly"},{"p":"<Unnamed>","l":"MLA"}];updateSearchResults();
